0faba4e7a392063091dd0c5d76b4307edca946c2
Reese Pollard <rpollard@gmail.com>
2022-07-27 17:31:12 -0700

    Implement uniface

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+const StatisticsLine = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0){
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  const all = good + neutral + bad;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tbody>
+      <StatisticsLine text="good" value={good} />
+      <StatisticsLine text="neutral" value={neutral} />
+      <StatisticsLine text="bad" value={bad} />
+      <StatisticsLine text="all" value={all} />
+      <StatisticsLine text="average" value={(good - bad) / (all)} />
+      <StatisticsLine text="positive" value={(good / all * 100 ) } />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const App = () => {
+  const [ good, setGood ] = useState(0)
+  const [ neutral, setNeutral ] = useState(0)
+  const [ bad, setBad ] = useState(0)
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
