6a70614e4301de7076a350c015b760cde81ab161
Reese Pollard <rpollard@gmail.com>
2022-07-28 22:07:32 -0700

    Add API call

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [ countries, setCountries] = useState([])
+  
+  const hook = () => axios
+    .get('https://restcountries.com/v3.1/all')
+    .then(response => setCountries(response.data))
+  console.log(countries)
+  useEffect(hook, [])
   return (
     <div>
+      Hello there
     </div>
   );
 }
