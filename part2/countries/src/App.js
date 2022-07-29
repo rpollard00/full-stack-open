86879b8c2685c9225837125dc382169b3775f4ae
Reese Pollard <rpollard@gmail.com>
2022-07-28 22:10:56 -0700

    Fix syntax error

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,11 +5,13 @@ import axios from 'axios'
 const App = () => {
   const [ countries, setCountries] = useState([])
   
-  const hook = () => axios
+  const hook = () => { axios
     .get('https://restcountries.com/v3.1/all')
     .then(response => setCountries(response.data))
-  console.log(countries)
+  }
+  
   useEffect(hook, [])
+  console.log(countries)
   return (
     <div>
       Hello there
