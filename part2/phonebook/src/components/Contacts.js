ea0937ecde85504af1857bc7b2df78640f09bedd
Reese Pollard <rpollard@gmail.com>
2022-07-31 17:11:24 -0700

    Add notifications

--- part2/phonebook/src/components/Contacts.js
+++ part2/phonebook/src/components/Contacts.js
@@ -7,17 +7,19 @@ const Contacts = (props) => {
         props.contacts.map((contact) => 
           <Contact
               key={contact.id}
+              id={contact.id}
               name={contact.name}
               phone={contact.phone} 
+              handleDelete={() => props.handleDelete(contact.id)}
           />)
       }
     </ul>
     )
 }
 
-const Contact = ({name, phone}) => {
+const Contact = ({name, phone, handleDelete, id}) => {
   return (
-    <li>{name} : {phone}</li>
+    <li>{name} : {phone}<button onClick={() => handleDelete(id)}>Delete</button></li>
   )
 }
 
