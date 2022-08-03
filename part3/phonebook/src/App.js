3220ce58e20c7f1899c4bdac9aa08179d4c40fce
Reese Pollard <rpollard@gmail.com>
2022-08-02 21:15:41 -0700

    Implement 3.19, 3.20

--- part3/phonebook/src/App.js
+++ part3/phonebook/src/App.js
@@ -1,109 +1,120 @@
-import './App.css';
+import "./App.css";
 
-import { useState, useEffect } from 'react';
-import Form from './components/Form'
-import Search from './components/Search'
-import Persons from './components/Persons'
-import phonebookService from './services/phonebookService';
-import Notification from './components/Notification';
-
+import { useEffect, useState } from "react";
+import Form from "./components/Form";
+import Notification from "./components/Notification";
+import Persons from "./components/Persons";
+import Search from "./components/Search";
+import phonebookService from "./services/phonebookService";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newPerson, setNewPerson] = useState('')
-  const [newPhone, setNewPhone] = useState('')
-  const [searchName, setSearchName] = useState('')
-  const [notificationMessage, setNotificationMessage] = useState(null)
-  const [notificationStyle, setNotificationStyle] = useState('informational')
+  const [persons, setPersons] = useState([]);
+  const [newPerson, setNewPerson] = useState("");
+  const [newPhone, setNewPhone] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [notificationMessage, setNotificationMessage] = useState(null);
+  const [notificationStyle, setNotificationStyle] = useState("informational");
 
   const hook = () => {
-    phonebookService
-      .getAll()
-      .then(personList => setPersons(personList))
-  }
+    phonebookService.getAll().then((personList) => setPersons(personList));
+  };
 
-  useEffect(hook, []) //empty array in 2nd element tells it to only fetch once
+  useEffect(hook, []); //empty array in 2nd element tells it to only fetch once
 
   const addNewPerson = (event) => {
-    event.preventDefault()
-    const existingPerson = persons.find((person) => (person.name.toLowerCase() === newPerson.toLowerCase()))
-  
+    event.preventDefault();
+    const existingPerson = persons.find(
+      (person) => person.name.toLowerCase() === newPerson.toLowerCase()
+    );
+
     if (existingPerson) {
       // update existing Person
-      updateHandler(existingPerson)
-    } 
-    else {
+      updateHandler(existingPerson);
+    } else {
       const personObject = {
         name: newPerson,
         phone: newPhone,
-      }
+      };
       phonebookService
         .postPerson(personObject)
-        .then(createdPerson => {
-          showMessage(`Added new Person ${createdPerson.name}`, 'informational')
-          return setPersons(persons.concat(createdPerson)
-        )})
-        
+        .then((createdPerson) => {
+          showMessage(
+            `Added new Person ${createdPerson.name}`,
+            "informational"
+          );
+          return setPersons(persons.concat(createdPerson));
+        })
+        .catch((error) => {
+          showMessage(error.response.data.error, "error");
+          console.log(error.response.data.error);
+        });
     }
-    
-    setNewPerson('')
-    setNewPhone('')
-  }
 
-  const updateHandler = personToUpdate => {
+    setNewPerson("");
+    setNewPhone("");
+  };
+
+  const updateHandler = (personToUpdate) => {
     if (window.confirm(`Update person ${personToUpdate.name}?`)) {
-      const personUpdateObj = { ...personToUpdate, phone: newPhone}
+      const personUpdateObj = { ...personToUpdate, phone: newPhone };
       phonebookService
         .updatePerson(personUpdateObj.id, personUpdateObj)
-        .then(updatedPerson =>  {
-          showMessage(`Updated person ${updatedPerson.name}: Phone ${updatedPerson.phone}`, 'informational')
-          return setPersons(persons.map(person => person.id !== updatedPerson.id ? person : updatedPerson ))
+        .then((updatedPerson) => {
+          showMessage(
+            `Updated person ${updatedPerson.name}: Phone ${updatedPerson.phone}`,
+            "informational"
+          );
+          return setPersons(
+            persons.map((person) =>
+              person.id !== updatedPerson.id ? person : updatedPerson
+            )
+          );
         })
-        .catch(error => {
-          showMessage(`Error ${personToUpdate.name} doesn't exist!`, 'error')
-          setPersons(persons.filter(person => person.id !== personToUpdate.id))
-      })
+        .catch((error) => {
+          console.log(error);
+          showMessage(error.response.data.error, "error");
+        });
     }
-  }
+  };
   const showMessage = (message, style) => {
-    setNotificationMessage(message)
-    setNotificationStyle(style)
-    setTimeout(() => setNotificationMessage(null), 5000)
-
-  }
+    setNotificationMessage(message);
+    setNotificationStyle(style);
+    setTimeout(() => setNotificationMessage(null), 5000);
+  };
   const deleteHandler = (id) => {
-    const personName = persons.find(c => c.id === id).name
+    const personName = persons.find((c) => c.id === id).name;
     if (window.confirm(`Do you want to delete ${personName}`)) {
       phonebookService
         .deletePerson(id)
         .then((response) => {
-          showMessage(`${personName} deleted successfully.`, 'informational')
-          return setPersons(persons.filter(person => person.id !== id))
+          showMessage(`${personName} deleted successfully.`, "informational");
+          return setPersons(persons.filter((person) => person.id !== id));
         })
-        .catch(error => {
-          showMessage(`Error ${personName} doesn't exist!`, 'error')
-          return setPersons(persons.filter(person => person.id !== id))
-        })
+        .catch((error) => {
+          showMessage(`Error ${personName} doesn't exist!`, "error");
+          return setPersons(persons.filter((person) => person.id !== id));
+        });
     }
-  }
+  };
 
-  const handleNameField = event => setNewPerson(event.target.value)
-  const handlePhoneField = event => setNewPhone(event.target.value)
-  const handleSearchField = (event) => setSearchName(event.target.value)
-  
+  const handleNameField = (event) => setNewPerson(event.target.value);
+  const handlePhoneField = (event) => setNewPhone(event.target.value);
+  const handleSearchField = (event) => setSearchName(event.target.value);
 
-  const personsToShow = persons
-    .filter(person => person.name.toLowerCase()
-    .includes(searchName.toLowerCase())
-    );
-  
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchName.toLowerCase())
+  );
+
   return (
     <div>
       <h1>Phonebook</h1>
-      <Notification message={notificationMessage} notificationStyle={notificationStyle}/>
-      <Search searchFieldHandler={handleSearchField} searchName={searchName}/>
+      <Notification
+        message={notificationMessage}
+        notificationStyle={notificationStyle}
+      />
+      <Search searchFieldHandler={handleSearchField} searchName={searchName} />
       <h1>add a new</h1>
-      <Form 
+      <Form
         nameFieldHandler={handleNameField}
         phoneFieldHandler={handlePhoneField}
         personName={newPerson}
@@ -111,11 +122,9 @@ const App = () => {
         personHandler={addNewPerson}
       />
       <h1>Numbers</h1>
-      <Persons
-        persons={personsToShow} 
-        handleDelete={deleteHandler}/>
+      <Persons persons={personsToShow} handleDelete={deleteHandler} />
     </div>
   );
-}
+};
 
 export default App;
--- part3/phonebook/src/services/phonebookService.js
+++ part3/phonebook/src/services/phonebookService.js
@@ -1,31 +1,29 @@
-import axios from 'axios'
-const baseUrl = "/api/persons"
+import axios from "axios";
+const baseUrl = "/api/persons";
 
 // getAll
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then((response) => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 // addPerson
 // post to the base url
 const postPerson = (personObj) => {
-  const request = axios.post(baseUrl, personObj)
-  return request.then(response => response.data)
-}
+  const request = axios.post(baseUrl, personObj);
+  return request.then((response) => response.data);
+};
 
 const deletePerson = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`)
-  return request.then(response => response)
-}
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response);
+};
 
 // updatePerson
 const updatePerson = (id, newPersonObj) => {
-  const request = axios.put(`${baseUrl}/${id}`, newPersonObj)
-  return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, newPersonObj);
+  return request.then((response) => response.data);
+};
 
-const exports = { getAll, postPerson, deletePerson, updatePerson }
+const exports = { getAll, postPerson, deletePerson, updatePerson };
 
-
-
-export default exports
+export default exports;
