8290cf71b33cb5b1cc1aa9810187bbbfcab2beee
Reese Pollard <rpollard@gmail.com>
2022-08-29 13:18:44 -0700

    8.9 Add books view

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -3,15 +3,18 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
-import { ALL_AUTHORS } from './queries'
+import { ALL_AUTHORS, ALL_BOOKS } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
-  const result = useQuery(ALL_AUTHORS)
+  const authors = useQuery(ALL_AUTHORS)
+  const books  = useQuery(ALL_BOOKS)
 
-  if  (result.loading) {
+  if  (authors.loading) {
     return <div>loading...</div>
   }
+
+  if (books.loading) return <div>loading ...</div>
 
   return (
     <div>
@@ -21,9 +24,9 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors authors={result.data.allAuthors} show={page === 'authors'} />
+      <Authors authors={authors.data.allAuthors} show={page === 'authors'} />
 
-      <Books show={page === 'books'} />
+      <Books books={books.data.allBooks} show={page === 'books'} />
 
       <NewBook show={page === 'add'} />
     </div>
