7bf797e5339b89623f520ffc1a686e16a8e16acc
Reese Pollard <rpollard@gmail.com>
2022-09-02 22:37:46 -0700

    8.19 Books by genre part 1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,4 +1,26 @@
-const Books = ({ show, books}) => {
+import { useEffect, useState } from 'react'
+
+const getGenres = (books) => {
+  console.log(books)
+  const genres = books
+    .map((b) => b.genres.filter(g => g !== "")) // filter blanks
+  console.log(genres)
+  return [...new Set(genres.flat()), 'all']
+}
+
+const Books = ({ show, books }) => {
+  const [genres, setGenres] = useState([])
+  const [genre, setGenre] = useState('all')
+
+  useEffect(() => {
+    setGenres(getGenres(books))
+  },[books])
+
+  const filterGenre = (event) => {
+    console.log('Target', event.target.textContent)
+    setGenre(event.target.textContent)
+  }
+
   if (!show) {
     return null
   }
@@ -10,11 +32,13 @@ const Books = ({ show, books}) => {
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>title</th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {books
+            .filter(b => genre !== 'all' ? b.genres.includes(genre) : b )
+            .map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -23,6 +47,9 @@ const Books = ({ show, books}) => {
           ))}
         </tbody>
       </table>
+      {genres.map((genre) => {
+        return <button onClick={filterGenre} key={genre}>{genre}</button>
+      })}
     </div>
   )
 }
